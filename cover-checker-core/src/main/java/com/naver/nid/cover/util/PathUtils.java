fe2f744b124fbd6e2264d48a87b4020c8cb39027
ksmail13 <ksmail13@gmail.com>
2021-01-11 02:46:22 +0900

    fix bug
    - do Matcher.quoteReplacement on generalizeSeparator for windows

--- cover-checker-core/src/main/java/com/naver/nid/cover/util/PathUtils.java
+++ cover-checker-core/src/main/java/com/naver/nid/cover/util/PathUtils.java
@@ -1,6 +1,22 @@
+/*
+	Copyright 2021 NAVER Corp.
+
+	Licensed under the Apache License, Version 2.0 (the "License");
+	you may not use this file except in compliance with the License.
+	You may obtain a copy of the License at
+
+		http://www.apache.org/licenses/LICENSE-2.0
+
+	Unless required by applicable law or agreed to in writing, software
+	distributed under the License is distributed on an "AS IS" BASIS,
+	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+	See the License for the specific language governing permissions and
+	limitations under the License.
+ */
 package com.naver.nid.cover.util;
 
 import java.io.File;
+import java.util.regex.Matcher;
 
 public class PathUtils {
     private PathUtils() {
@@ -25,6 +41,6 @@ public class PathUtils {
      */
     public static String generalizeSeparator(String path) {
         if (path == null || path.isEmpty()) return path;
-        return path.replaceAll("[\\\\/]", File.separator);
+        return path.replaceAll("[\\\\/]", Matcher.quoteReplacement(File.separator));
     }
 }
